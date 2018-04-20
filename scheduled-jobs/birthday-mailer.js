dcf308849dd1442bb31062e12a36e0621ac4bd75
Anton Karytka <antnkrtk@gmail.com>
2018-04-21 00:06:48 +0300

    Add sending emails to birthday guys and schedule it for 13:00 every day.

--- scheduled-jobs/birthday-mailer.js
+++ scheduled-jobs/birthday-mailer.js
@@ -1,13 +1,16 @@
+const Promise = require('bluebird');
 const schedule = require('node-schedule');
+const nodemailer = require('nodemailer');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
 const models = require('../models');
 
-const emailCongratulation = schedule.scheduleJob({hour: 13}, () => {
+
+const schenduleBirthdayEmailing = () => schedule.scheduleJob({hour: 13}, () => {
   const currentDate = new Date();
 
-  return models.User.find({
+  return models.User.findAll({
     where: {
       [Op.and]: [
         Sequelize.literal(`EXTRACT(MONTH FROM "birthDate") = ${currentDate.getMonth() + 1}`),
@@ -15,5 +18,34 @@ const emailCongratulation = schedule.scheduleJob({hour: 13}, () => {
       ]
     }
   })
-  .then(users => console.log(users))
+  .then((users = []) => users.length && Promise.each(users, user => {
+    nodemailer.createTestAccount((err, account) => {
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+          user: account.user,
+          pass: account.pass
+        }
+      });
+
+      const mailOptions = {
+        from: '"Musicflow Documents" <antnkrtk@gmail.com>',
+        to: `${user.email}`,
+        subject: 'Happy Birthday!',
+        html: '<b>Hello world?</b>'
+      };
+
+      return transporter.sendMail(mailOptions, (error, info) => {
+        if (error) return console.log(error);
+
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+      });
+    });
+  }))
 });
+
+
+module.exports = schenduleBirthdayEmailing;
